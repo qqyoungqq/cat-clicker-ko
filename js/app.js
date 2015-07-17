218079c652d6631fd8c52c9369e29b62693fae9a
qqyoungqq <qianyang@mail.usf.edu>
2015-07-17 15:20:48 -0400

    feat: Separate out the cat Model

--- js/app.js
+++ js/app.js
@@ -1,13 +1,10 @@
-var ViewModel = function() {
+/* Model */
+var Cat = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 	this.imgAttribution = ko.observable('http://www.flicker.com/photos/');
 	this.nicknames = ko.observable(['Tabtab','T-Bone','Mr. T','Tabitha Tab Tabby Catty Cat']);
-	
-	this.incrementCounter = function() {
-		this.clickCount(this.clickCount()+1);
-	}; // end incrementCounter
 
 	this.level = ko.computed(function(){
 		var level;
@@ -25,7 +22,17 @@ var ViewModel = function() {
 		}
 		return level;
 	}, this); // end level
+} // end Cat
 
-} // ViewModel
+/* ViewModel */
+var ViewModel = function() {
+
+	this.currentCat = ko.observable( new Cat() );
+
+	this.incrementCounter = function() {
+		this.currentCat().clickCount(this.currentCat().clickCount()+1);
+	}; // end incrementCounter
+
+} // end ViewModel
 
 ko.applyBindings(new ViewModel())
