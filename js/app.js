e28d16aca03b84d59e180a253a58ac751c7ec25c
qqyoungqq <qianyang@mail.usf.edu>
2015-07-17 14:36:14 -0400

    feat: Implement levels to the cat

--- js/app.js
+++ js/app.js
@@ -3,9 +3,15 @@ var ViewModel = function() {
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 	this.imgAttribution = ko.observable('http://www.flicker.com/photos/');
-
+	this.level = ko.observable('Newborn');
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount()+1);
+		if (this.clickCount()>10) {
+			this.level('Infant');
+		} 
+		if (this.clickCount()>30) {
+			this.level('Teen');
+		}
 	};
 }
 
