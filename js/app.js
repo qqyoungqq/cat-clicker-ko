e8247225bc149646782ff8ea7cd333f81129a741
qqyoungqq <qianyang@mail.usf.edu>
2015-07-17 15:11:11 -0400

    feat: Implement levels to the cat using ko Computed Observables; Group the levels inside a div

--- js/app.js
+++ js/app.js
@@ -3,18 +3,29 @@ var ViewModel = function() {
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 	this.imgAttribution = ko.observable('http://www.flicker.com/photos/');
-	this.level = ko.observable('Newborn');
 	this.nicknames = ko.observable(['Tabtab','T-Bone','Mr. T','Tabitha Tab Tabby Catty Cat']);
 	
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount()+1);
-		if (this.clickCount()>10) {
-			this.level('Infant');
-		} 
-		if (this.clickCount()>30) {
-			this.level('Teen');
-		}
 	}; // end incrementCounter
-}
+
+	this.level = ko.computed(function(){
+		var level;
+		var click = this.clickCount();
+		if (click < 10) {
+			level = 'Newborn';
+		} else if (click < 20) {
+			level = 'Infant';
+		} else if (click < 30) {
+			level = 'Child';
+		} else if (click < 40) {
+			level = 'Teen';
+		} else {
+			level = 'Adult';
+		}
+		return level;
+	}, this); // end level
+
+} // ViewModel
 
 ko.applyBindings(new ViewModel())
